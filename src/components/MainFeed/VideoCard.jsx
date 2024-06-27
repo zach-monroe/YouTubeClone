e8b7217bb0d01a9985cbca112ee3fadf6f4119f6
Zach Monroe <z@localhost.localdomain>
2024-06-27 16:32:58 -0400

    Tweaks

--- src/components/MainFeed/VideoCard.jsx
+++ src/components/MainFeed/VideoCard.jsx
@@ -3,7 +3,7 @@ const VideoCard = ({ thumb, videoId, title, desc }) => {
     <div>
       <a href={`www.youtube/watch?v=${videoId}`}>
         <img src={thumb} /></a>
-      <h3 className="truncate">{title}</h3>
+      <h3 className="truncate">{decodeHTMLEntities(JSON.parse(title))}</h3>
       <p className="truncate">{desc}</p>
     </div>
 
